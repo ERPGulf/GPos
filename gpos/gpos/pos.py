ae076f9f51cbff99f6ef8759f3277ab9970fa8c0
Aysha-sithara <asithara@htsqatar.com>
2025-08-08 15:43:23 +0300

    Added feature for price-list based on pos-profile

--- gpos/gpos/pos.py
+++ gpos/gpos/pos.py
@@ -303,9 +303,9 @@ def get_items(item_group=None, last_updated_time=None, pos_profile = None):
             ],
         )
         # price_list = frappe.db.get_value("POS Profile", pos_profile, "selling_price_list")
-        price_list = "Standard Selling3"
+        price_list = "Standard Selling"
         if pos_profile:
-            price_list = frappe.db.get_value("POS Profile", pos_profile, "selling_price_list") or "Standard Selling3"
+            price_list = frappe.db.get_value("POS Profile", pos_profile, "selling_price_list") or "Standard Selling"
         frappe.log_error(pos_profile)
         frappe.log_error(price_list)
         item_prices = frappe.get_all(
