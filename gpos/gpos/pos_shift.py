11fe115692cd38bd7f401eb8bbd76152df409865
Harshafiroz <harsha@htsqatar.com>
2026-06-02 14:20:56 +0300

    updated pos shift api

--- gpos/gpos/pos_shift.py
+++ gpos/gpos/pos_shift.py
@@ -190,7 +190,7 @@ def closing_shift(
 
             mode = payment.get("mode_of_payment", "").strip()
             original_mode = mode.lower()
-            if original_mode == "claimed_loyalty":
+            if original_mode == "Loyalty":
                 original_mode = "loyalty"
 
             if original_mode in ["cash", "card","loyalty"] and pos_profile_doc:
